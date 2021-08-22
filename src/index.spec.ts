0955db88c33a09268c17ea9b0b03b7876f5d6fd4
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-22 09:14:49 +0800

    Add() returns zero for empty string or no input

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,10 @@
 import { add } from "./index";
 
 describe("Test add function", () => {
-  it("should do something", () => {});
+  it("should return 0 when input is an empty string", () => {
+    expect(add("")).toEqual(0);
+  });
+  it("should return 0 when no input provided", () => {
+    expect(add()).toEqual(0);
+  });
 });
