86a9618d4b5756e8bb2ee0973390f0c87c1e723d
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-22 09:54:50 +0800

    Extracted customDelimiter() from add()

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 export function add(input?: string): number {
   if (input) {
     if (input.substring(0, 2) == "//") {
-      const delimiter: string = input.charAt(2);
-      const splitString: string = input.split("\n")[1];
-      return calculation(splitString, delimiter);
+      return customDelimiter(input);
     } else {
       const delimiter: RegExp = /\n|,/;
       return calculation(input, delimiter);
     }
   } else return 0;
+
+  function customDelimiter(input: string): number {
+    const delimiter: string = input.charAt(2);
+    const splitString: string = input.split("\n")[1];
+    return calculation(splitString, delimiter);
+  }
 }
 
 function calculation(input: string, delimiter: RegExp | string): number {
