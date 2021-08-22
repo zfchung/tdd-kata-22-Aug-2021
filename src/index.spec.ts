78b35b4a75c2c485a456fcb5ce8006aa911a4d77
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-22 09:17:35 +0800

    Add() function returns summation of 1 number

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,4 +7,9 @@ describe("Test add function", () => {
   it("should return 0 when no input provided", () => {
     expect(add()).toEqual(0);
   });
+  it("should return summation of 1 number", () => {
+    expect(add("1")).toEqual(1);
+    expect(add("10")).toEqual(10);
+    expect(add("23")).toEqual(23);
+  });
 });
