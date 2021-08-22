33807d391cfaf207c508adf8b325e8c170b78a87
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-22 09:51:43 +0800

    Extracted calculation() function from add() that can be reused in IF statement

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,17 @@ export function add(input?: string): number {
     if (input.substring(0, 2) == "//") {
       const delimiter: string = input.charAt(2);
       const splitString: string = input.split("\n")[1];
-      const result = splitString
-        .split(delimiter)
-        .map(Number)
-        .reduce((accumulator, currentValue) => accumulator + currentValue);
-      return result;
+      return calculation(splitString, delimiter);
     } else {
       const delimiter: RegExp = /\n|,/;
-      const result = input
-        .split(delimiter)
-        .map(Number)
-        .reduce((accumulator, currentValue) => accumulator + currentValue);
-      return result;
+      return calculation(input, delimiter);
     }
   } else return 0;
+}
+
+function calculation(input: string, delimiter: RegExp | string): number {
+  return input
+    .split(delimiter)
+    .map(Number)
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
 }
